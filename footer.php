4ec7ac8acd93180d5c3a05b4147759d32c826993
razafindrambola <nilaina.razafindrambola@etu.unistra.fr>
2024-02-28 23:06:06 +0100

    ajout php

---
+++ footer.php
@@ -0,0 +1,102 @@
+<footer class="footer">
+    <section class="info">
+        <section class="name">
+            <h3>Nilaina </br>Razafindrambola</h3>
+        </section>
+
+        <section class="contact">
+            <h3>Contact</h3>
+            <ul class="network">
+                <li><a href="https://www.facebook.com/nilaina.razafindrambola/?paipv=0&eav=AfYGd1rpvCasm83T60uCInPx-4fZfLCdZVI_Cf-_8qLIEOOqeXeceZi1S7YCDwRJkK4&_rdr"><i class="fa fa-brands fa-facebook"></i></a></li>
+                <li><a href="https://www.instagram.com/?hl=fr"><i class="fa fa-brands fa-instagram"></i></a></li>
+                <li><a href="https://twitter.com/?lang=fr"><i class="fa fa-brands fa-twitter"></i></a></li>
+                <li><a href="mailto:nilainarazaf@gmail.com"><i class="fa fa-brands fa-google"></i></a></li>
+                <li><a href="https://fr.linkedin.com/in/nilaina-razafindrambola-0650b9225"><i class="fa fa-brands fa-linkedin"></i></a></li>
+            </ul>
+            <ul class="phone">
+                <li><i class="fa-solid fa-phone"></i> +33 (0)7 67 22 50 44</li>
+            </ul>
+        </section>
+    </section>
+    <section class="copyright">@copyrigth2024 - tout droit reserve<section>
+</footer>
+
+
+<style>
+
+/*/////////////FOOTER BEGIN///////////////*/
+footer{
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    background-color: rgb(22, 22, 22);
+    color: #a1a1a1;
+    font-size: medium;
+}
+footer h3{
+    text-decoration: none;
+    color: #a1a1a1;
+}
+.info{
+    display: flex;
+    flex-direction: row;
+    padding: 20px;
+    margin: 10px 0;
+}
+.copyright{
+    text-align: center;
+    font-size: 0.8em;
+    padding: 5px;
+}
+.name{
+    display: flex;
+    justify-content: center;
+    margin-top: 50px;
+    width: 30vw;
+    cursor: pointer;
+}
+.contact {
+    border-left: 2px solid #a1a1a1;
+    display: flex;
+    flex-direction: column;
+    margin-left: 10vw;
+    padding-left: 10vw;
+}
+.network{
+    display: flex;
+    flex-direction: row;
+}
+.network li, .phone li{
+    list-style: none;
+    margin: 0 10px;
+}
+
+
+.fa {
+    color : #a1a1a1;
+}
+.network .fa:hover{
+    position: relative;
+    transform: scale(1.4) translateY(-20%);
+}
+.fa-facebook:hover {
+    color: #3b5998;
+}
+.fa-twitter:hover {
+    color: #5d8ffa;
+}
+.fa-instagram:hover {
+    color: #e64545;
+}
+.fa-google:hover {
+    color: #bd1111;
+}
+.fa-linkedin:hover {
+    color: #2851a8;
+}
+.copyright{
+    text-align: center;
+    font-size: medium;
+}
+/*/////////////FOOTER END///////////////*/
+</style>
