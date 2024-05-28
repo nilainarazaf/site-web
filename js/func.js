adc311dfb6ebd932203498e45afb29bdd911bc66
razafindrambola <nilaina.razafindrambola@etu.unistra.fr>
2024-05-28 09:19:42 +0200

    màj 28-05

--- js/func.js
+++ js/func.js
@@ -1,128 +1,42 @@
-const projects = [
-    {
-        "title": "Gestionnaire de tâches (To-Do List)",
-        "image": "img/to_do.png",
-        "description": "Une application web permettant de gérer une liste de tâches à faire.",
-        "features": [
-            "Ajouter, supprimer et marquer des tâches comme terminées.",
-            "Utilisation de HTML pour la structure, CSS pour le style et JavaScript pour la logique de gestion des tâches."
-        ],
-        "modalContent": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac tortor vitae velit malesuada efficitur id sed neque. ...",
-        "modalImage": "img/to_do.png",
-        "modalId": "project1",
-        "extend": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac tortor vitae velit malesuada efficitur id sed neque. Donec quis tellus a arcu volutpat vehicula. Quisque auctor risus at turpis vehicula faucibus. Proin sit amet ligula eget arcu viverra posuere. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed gravida arcu at leo condimentum ullamcorper. Nunc dictum mi in dolor consequat, sed faucibus augue convallis. Suspendisse sit amet lacinia risus. Sed nec aliquet lectus. Vivamus auctor nisi a enim sodales, at ullamcorper tortor volutpat.",
-        "link": "https://git.unistra.fr/razafindrambola/"
-    },
-    {
-        "title": "Blog Personnel",
-        "image": "img/blog.png",
-        "description": "Un site web où l'utilisateur peut publier des articles, les commenter et les partager.",
-        "features": [
-            "Système d'authentification, CRUD (Création, Lecture, Mise à jour, Suppression) des articles, système de commentaires.",
-            "Utilisation de PHP et MySQL pour la gestion des données, HTML/CSS/JavaScript pour l'interface utilisateur."
-        ],
-        "modalContent": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus non ipsum nec mauris convallis suscipit. ...",
-        "modalImage": "img/blog.png",
-        "modalId": "project2",
-        "extend": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac tortor vitae velit malesuada efficitur id sed neque. Donec quis tellus a arcu volutpat vehicula. Quisque auctor risus at turpis vehicula faucibus. Proin sit amet ligula eget arcu viverra posuere. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed gravida arcu at leo condimentum ullamcorper. Nunc dictum mi in dolor consequat, sed faucibus augue convallis. Suspendisse sit amet lacinia risus. Sed nec aliquet lectus. Vivamus auctor nisi a enim sodales, at ullamcorper tortor volutpat.",
-        "link": "https://git.unistra.fr/razafindrambola/"
-    },
-    {
-        "title": "Calculatrice en ligne",
-        "image": "img/calc.png",
-        "description": "Une application web pour effectuer des opérations mathématiques de base.",
-        "features": [
-            "Addition, soustraction, multiplication, division, gestion des décimales.",
-            "Utilisation de HTML pour les boutons et l'affichage, JavaScript pour la logique de calcul."
-        ],
-        "modalContent": "Maecenas tempus, lacus in maximus volutpat, sapien ex vehicula dolor, nec convallis dui odio ac enim. Aliquam convallis scelerisque nisl sit amet eleifend. ...",
-        "modalImage": "img/calc.png",
-        "modalId": "project3",
-        "extend": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac tortor vitae velit malesuada efficitur id sed neque. Donec quis tellus a arcu volutpat vehicula. Quisque auctor risus at turpis vehicula faucibus. Proin sit amet ligula eget arcu viverra posuere. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed gravida arcu at leo condimentum ullamcorper. Nunc dictum mi in dolor consequat, sed faucibus augue convallis. Suspendisse sit amet lacinia risus. Sed nec aliquet lectus. Vivamus auctor nisi a enim sodales, at ullamcorper tortor volutpat.",
-        "link": "https://git.unistra.fr/razafindrambola/"
-    },
-    {
-        "title": "Galerie d'images",
-        "image": "img/galerie.png",
-        "description": "Une galerie d'images en ligne où les utilisateurs peuvent visualiser et télécharger des images.",
-        "features": [
-            "Affichage des images, possibilité de zoomer, téléchargement des images.",
-            "Utilisation de HTML/CSS pour la mise en page, JavaScript pour la fonctionnalité de zoom et de téléchargement."
-        ],
-        "modalContent": "Pellentesque habitant morbi maximus volutpat, sapien ex vehicula dolor, nec convallis dui odio ac enim. Aliquam convallis scelerisque nisl sit amet eleifend. ...",
-        "modalImage": "img/galerie.png",
-        "modalId": "project4",
-        "extend": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac tortor vitae velit malesuada efficitur id sed neque. Donec quis tellus a arcu volutpat vehicula. Quisque auctor risus at turpis vehicula faucibus. Proin sit amet ligula eget arcu viverra posuere. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed gravida arcu at leo condimentum ullamcorper. Nunc dictum mi in dolor consequat, sed faucibus augue convallis. Suspendisse sit amet lacinia risus. Sed nec aliquet lectus. Vivamus auctor nisi a enim sodales, at ullamcorper tortor volutpat.",
-        "link": "https://git.unistra.fr/razafindrambola/"
-    },
-    {
-        "title": "Jeu de mots cachés",
-        "image": "img/mot.png",
-        "description": "Un jeu de mots cachés où les joueurs doivent trouver des mots dans une grille de lettres.",
-        "features": [
-            "Génération aléatoire de la grille, vérification des mots trouvés, système de points.",
-            "Utilisation de JavaScript pour générer la grille et vérifier les réponses des joueurs."
-        ],
-        "modalContent": "Lacus in maximus volutpat, sapien ex vehicula dolor, nec convallis dui odio ac enim. Aliquam convallis scelerisque nisl sit amet eleifend. ...",
-        "modalImage": "img/mot.png",
-        "modalId": "project5",
-        "extend": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac tortor vitae velit malesuada efficitur id sed neque. Donec quis tellus a arcu volutpat vehicula. Quisque auctor risus at turpis vehicula faucibus. Proin sit amet ligula eget arcu viverra posuere. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed gravida arcu at leo condimentum ullamcorper. Nunc dictum mi in dolor consequat, sed faucibus augue convallis. Suspendisse sit amet lacinia risus. Sed nec aliquet lectus. Vivamus auctor nisi a enim sodales, at ullamcorper tortor volutpat.",
-        "link": "https://git.unistra.fr/razafindrambola/"
-    },
-    {
-        "title": "Application de météo",
-        "image": "img/meteo.png",
-        "description": "Une application web qui fournit des informations météorologiques en temps réel.",
-        "features": [
-            "Affichage de la température, de l'humidité, des prévisions météo, localisation automatique.",
-            "Utilisation d'une API météo pour récupérer les données, HTML/CSS/JavaScript pour l'interface utilisateur et l'affichage des informations."
-        ],
-        "modalContent": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac tortor vitae velit malesuada efficitur id sed neque. ...",
-        "modalImage": "img/meteo.png",
-        "modalId": "project6",
-        "extend": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac tortor vitae velit malesuada efficitur id sed neque. Donec quis tellus a arcu volutpat vehicula. Quisque auctor risus at turpis vehicula faucibus. Proin sit amet ligula eget arcu viverra posuere. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed gravida arcu at leo condimentum ullamcorper. Nunc dictum mi in dolor consequat, sed faucibus augue convallis. Suspendisse sit amet lacinia risus. Sed nec aliquet lectus. Vivamus auctor nisi a enim sodales, at ullamcorper tortor volutpat.",
-        "link": "https://git.unistra.fr/razafindrambola/"
-    }
-]
-    
+
 
 /////////////////////////////////////
 
 /////////////////////////////////////
 // Chargement du fichier JSON avec les projets
-function loadProjects() {
-    const selection = document.getElementById('selection');
+// function loadProjects() {
+//     const selection = document.getElementById('selection');
 
-    // Parcourir les projets et les ajouter au conteneur
-    projects.forEach(project => {
-        const projectHTML = `
-            <article class="box">
-                <section class="img"><img src="${project.image}" alt="illustration"></section>
-                <section class="text">
-                    <h2>${project.title}</h2>
-                    <p>${project.description}</p>
-                    <p>${project.features}</p>
-                    <p class="read-more modal-trigger">Plus de détails<span class="arrow"></span></p>
+//     // Parcourir les projets et les ajouter au conteneur
+//     projects.forEach(project => {
+//         const projectHTML = `
+//             <article class="box">
+//                 <section class="img"><img src="${project.image}" alt="illustration"></section>
+//                 <section class="text">
+//                     <h2>${project.title}</h2>
+//                     <p>${project.description}</p>
+//                     <p>${project.features}</p>
+//                     <p class="read-more modal-trigger">Plus de détails<span class="arrow"></span></p>
                     
-                    <!-- Fenêtre modal en js -->
-                    <article class="modal-container" id="${project.modalId}">
-                        <div class="overlay modal-trigger"></div>
-                        <div class="modal" role="dialog">
-                            <i class="close-modal modal-trigger fa-solid fa-circle-xmark"></i>
-                            <section><img src="${project.modalImage}" alt="illustration"></section>
-                            <h2>${project.title}</h2>
-                            <article>${project.modalFurtherDescription}</article>
-                            <p>ref : <a href="${project.modalLink}">${project.modalLink}</a></p>
-                        </div>
-                    </article>
-                    <!-- /////////////////// -->
+//                     <!-- Fenêtre modal en js -->
+//                     <article class="modal-container" id="${project.modalId}">
+//                         <div class="overlay modal-trigger"></div>
+//                         <div class="modal" role="dialog">
+//                             <i class="close-modal modal-trigger fa-solid fa-circle-xmark"></i>
+//                             <section><img src="${project.modalImage}" alt="illustration"></section>
+//                             <h2>${project.title}</h2>
+//                             <article>${project.modalFurtherDescription}</article>
+//                             <p>ref : <a href="${project.modalLink}">${project.modalLink}</a></p>
+//                         </div>
+//                     </article>
+//                     <!-- /////////////////// -->
 
-                </section>
-            </article>
-        `;
-        selection.innerHTML += projectHTML;
-    });
-}
+//                 </section>
+//             </article>
+//         `;
+//         selection.innerHTML += projectHTML;
+//     });
+// }
 
 // Affichage des modifications lors de l'intersection avec les éléments
 function setupIntersectionObserver() {
@@ -172,42 +86,64 @@ function displayFullName() {
             }
         }
     };
-
     displayNextCharacter(); // Appel initial de la fonction
 }
 
-// Gestion des événements click sur les boutons "Plus de détails"
-function setupModalTriggers() {
-    const modalTriggers = document.querySelectorAll(".read-more.modal-trigger");
 
-    // Ajoutez un écouteur d'événements click à chaque bouton
-    modalTriggers.forEach(trigger => {
-        trigger.addEventListener("click", () => {
-            // Selectionnez le modal correspondant à ce bouton
-            const modalId = trigger.closest('.box').querySelector('.modal-container').getAttribute('id');
-            const modal = document.getElementById(modalId);
+//Animation des projets
+function projectAnime(){
+    const ul = document.querySelector('.project ul');
+    const slides = document.querySelectorAll('.project ul li');
+    const prevButton = document.getElementById('prev');
+    const nextButton = document.getElementById('next');
+    const indicatorsContainer = document.querySelector('.indicators');
+    let index = 0;
+    const totalSlides = slides.length;
 
-            modal.classList.add("active");
-            event.preventDefault();
-            console.log(modal.classList);
+    // Crée les points d'indication
+    slides.forEach((slide, i) => {
+        const indicator = document.createElement('div');
+        if (i === 0) {
+            indicator.classList.add('active');
+        }
+        indicator.addEventListener('click', () => {
+            index = i;
+            updateSlides();
         });
+        indicatorsContainer.appendChild(indicator);
+    });
+    const indicators = document.querySelectorAll('.indicators div');
+
+    function showSlide(i) {
+        ul.style.transform = `translateX(-${i * 100}%)`;
+    }
+
+    function updateSlides() {
+        showSlide(index);
+        indicators.forEach((indicator, i) => {
+            indicator.classList.toggle('active', i === index);
+        });
+    }
+
+    function nextSlide() {
+        index = (index + 1) % totalSlides;
+        updateSlides();
+    }
+
+    function prevSlide() {
+        index = (index - 1 + totalSlides) % totalSlides;
+        updateSlides();
+    }
+
+    nextButton.addEventListener('click', () => {
+        nextSlide();
+    });
+    prevButton.addEventListener('click', () => {
+        prevSlide();
     });
 }
 
-// Gestion des événements click sur les boutons de fermeture de modal
-function setupCloseButtons() {
-    // Selectionnez tous les boutons de fermeture de modal
-    const closeButtons = document.querySelectorAll(".close-modal");
-
-    closeButtons.forEach(button => {
-        button.addEventListener("click", () => {
-            const modal = button.closest('.modal-container');
-            modal.classList.remove("active");
-        });
-    });
-}
-
-// Affichage sticky de l'élément intro
+// Affichage sticky de l'element intro
 function setupStickyIntro() {
     window.addEventListener('scroll', function() {
         const introElement = document.querySelector('.project .intro');
@@ -221,14 +157,14 @@ function setupStickyIntro() {
     });
 }
 
+
 // Fonction principale pour initialiser toutes les fonctionnalités
 function initialize() {
     // loadProjects(); // Chargement des projets
     setupIntersectionObserver(); // Observateur d'intersection
     displayFullName(); // Affichage du nom
-    // setupModalTriggers(); // Gestion des boutons "Plus de détails"
-    setupCloseButtons(); // Gestion des boutons de fermeture de modal
-    setupStickyIntro(); // Affichage sticky de l'élément intro
+    projectAnime(); //Anime les projets
+    setupStickyIntro(); //
 }
 
 // Exécution de la fonction initialize après le chargement complet du DOM
